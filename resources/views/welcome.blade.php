7fd79955004277d9dd883543ca93d9c61e0dfbcd
Ronney Dutra <rbdutra@gmail.com>
2025-06-26 14:11:40 -0300

    Menu na página welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,14 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        Welcome
-        {{-- <h1>{{ $title }}</h1>
-        <h3>{{ $description }}</h3>
-        <h3>{{ $nome }}</h3> --}}
+        <div class="max-w-2xl mx-auto">
+            <h1 class="text-3xl font-bold mb-4">Bem-vindo ao Curso Laravel</h1>
+            <nav class="mb-4">
+                <ul class="flex space-x-4">
+                    <li><a href="{{ route('aluno.index') }}" class="text-blue-500 hover:underline">Alunos</a></li>
+                    <li><a href="{{ route('curso.index') }}" class="text-blue-500 hover:underline">Cursos</a></li>
+                </ul>
+            </nav>
+        </div>
     </body>
 </html>
